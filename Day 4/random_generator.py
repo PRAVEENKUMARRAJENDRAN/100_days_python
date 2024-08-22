1872640968318edb348faed37f9aae55ae2245b1
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-08-21 21:07:15 -0400

    random module generator

---
+++ Day 4/random_generator.py
@@ -0,0 +1,10 @@
+import random
+
+#generates the random whole number between the range 1 to 15
+print(random.randint(1, 15))
+
+#generates the real number between 0 to 1
+print(random.random())
+
+#generates the real number between the 1 to 100
+print(random.uniform(1,100))
