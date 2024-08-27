f95d2ca90028599b9ed2dc96afb321e0658f92fe
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-08-26 21:52:26 -0400

    Life of weeks

---
+++ Day 8/life_in_weeks.py
@@ -0,0 +1,16 @@
+from datetime import *
+
+def life_in_weeks(age):
+    remaining_age = 90 - age
+    print(f"You have {remaining_age * 52} weeks left")
+
+    
+
+
+
+
+
+
+my_age = int(input("Enter your age"))
+#my_age = 25
+life_in_weeks(my_age)
