dda3f3818130f3a2ed93ed0e307ce9e261ad8ea3
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-08-26 21:30:01 -0400

    Rough Work

---
+++ rough_work.py
@@ -0,0 +1,11 @@
+
+index_postion = []
+l = 'a'
+arr_list = ['a', 'b', 'c', 'a', 'a']
+for index, letter in enumerate(arr_list):
+    if l == letter:
+        index_postion.append(index)
+
+print(index_postion)
+
+thecodont
