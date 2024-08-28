028fb47e66a8abba46807ab60d8e40cedfb16cbe
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-08-27 21:09:23 -0400

    Serious Note Day 9

--- Day 9/basic_dictionary.py
+++ Day 9/basic_dictionary.py
@@ -4,9 +4,16 @@ my_dict = dict()
 my_dict["name"] = "praveen"
 my_dict["age"] = 25
 my_dict["profession"] = "Data Engineer"
-my_dict["high_degree"] = "phd"
+my_dict["high_degree"] = "Phd in Data Management"
 
 print(my_dict)
 
 for index, (key, value )in enumerate(my_dict.items()):
-    print(key, value)
+    if key == "name":
+        print(f"Hi! I'm {value}")
+    elif key == "age":
+        print(f"I'm {value} years old.")
+    elif key == "profession":
+        print(f"I'm a {value}.")
+    else:
+        print(f"I have done {value}.")
