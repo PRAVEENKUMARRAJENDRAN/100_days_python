1f82c3a63bb76b10504abf396d1bfcfde100536d
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-08-30 19:04:31 -0400

    Day 12 prime number

---
+++ Day 12/prime_number_check.py
@@ -0,0 +1,15 @@
+def prime(num):
+    flag = False
+    if num == 0 or num == 1:
+        print(f"{num} is not a prime number")
+    elif num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                flag = True
+                break
+        if flag:
+            print(f"{num} is not a prime number")
+        else:
+            print(f"{num} is a prime number")
+
+prime(5)
