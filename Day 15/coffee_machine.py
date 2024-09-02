8ce3feab24e035b8e6d3a641d3365ad82b2d8d2d
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2024-09-02 18:49:14 -0400

    Day 15 Inital Push

---
+++ Day 15/coffee_machine.py
@@ -0,0 +1,65 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+money = {
+    "value": 0,
+}
+
+
+def report_output():
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${money['value']}")
+
+
+
+
+
+def coffee_make():
+
+
+    on = True
+
+    while on:
+        coffee = input("What would you like? (espresso/latte/cappuccino):")
+
+        if coffee == 'report':
+            report_output()
+        elif coffee == 'off':
+            on = False
+            print("Coffee machine is under maintenance. Please try again after an hour.")
+        else:
+            print("Coffee Selection")
+
+
+coffee_make()
